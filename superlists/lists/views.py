d0ba33630726c4c77692e06f9053796a2c340027
Nguyen Tuan Anh <tuananh.set.hut@gmail.com>
2017-10-23 17:16:39 +0900

    differ between request.POST and request.POST.get

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 # Create your views here.
 def home_page(request):
-    return render(request, 'home.html')
+    # differ between request.POST and request.POST.get
+    # link: https://stackoverflow.com/questions/12518517/request-post-getsth-vs-request-poststh-difference
+    # using request.POST.get
+    # return render(request, 'home.html', {
+    #     'new_item_text': request.POST['item_text'],
+    # })
+
+    # using request.POST
+    if 'item_text' in request.POST:
+        return render(request, 'home.html', {
+            'new_item_text': request.POST['item_text'],
+        })
+    else:
+        return render(request, 'home.html', {
+            'new_item_text': '',
+        })
+
